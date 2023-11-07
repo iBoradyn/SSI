9d83a0a5cfdeebbd23ae68d988eef583b05a61f7
i_boradyn <igor.boradyn@gmail.com>
2023-11-07 17:45:57 +0100

    lab5

--- blok2/lab5/fireflies.py
+++ blok2/lab5/fireflies.py
@@ -4,8 +4,6 @@ from typing import Callable
 import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib import cm
-from matplotlib.colors import LightSource
-from matplotlib.ticker import LinearLocator
 
 
 class Fireflies:
@@ -49,9 +47,6 @@ class Fireflies:
         self.plot3D(F, X)
 
     def plot3D(self, F: Callable, X: list):
-        fig, axs = plt.subplots(3, 2, subplot_kw={"projection": "3d"})
-
-        # Make data.
         x = np.arange(self.x_min, self.x_max, .25)
         y = np.arange(self.x_min, self.x_max, .25)
         x, y = np.meshgrid(x, y)
@@ -60,31 +55,16 @@ class Fireflies:
 
         x_flies = list(zip(*X))[0]
         y_flies = list(zip(*X))[1]
-        z_flies = list(map(lambda x, y: F(x, y), x_flies, y_flies))
 
-        # Plot the surface.
-        for i in range(3):
-            for j in range(2):
-                axs[i][j].scatter(
-                    x_flies, y_flies, z_flies, color='green', s=50,
-                )
-                surf = axs[i][j].plot_surface(
-                    x, y, z, cmap=cm.coolwarm, linewidth=2, antialiased=False,
-                )
+        fig, ax = plt.subplots()
+        CS = ax.contour(x, y, z)
+        ax.clabel(CS, inline=True, fontsize=10)
+        ax.autoscale(False)
+        ax.scatter(
+            x_flies, y_flies, color='red', zorder=1,
+        )
 
-                # fig.colorbar(surf, shrink=0.5, aspect=5)
-
-        fig.subplots_adjust(right=0.8)
-        cbar_ax = fig.add_axes([0.85, 0.15, 0.05, 0.7])
-        fig.colorbar(surf, cax=cbar_ax)
         fig.legend(labels=['Fireflies'])
-
-        axs[0][0].view_init(30, 0)
-        axs[0][1].view_init(30, 60)
-        axs[1][0].view_init(30, 120)
-        axs[1][1].view_init(30, 180)
-        axs[2][0].view_init(30, 240)
-        axs[2][1].view_init(30, 300)
 
         plt.show()
 
